fd63441a85d471bd58c88d346239b46839ecaa07
Fatma <gormezfatma73@.de>
2026-06-01 20:26:40 +0200

    Add test for quality not below zero

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -11,4 +11,12 @@ describe("Gilded Rose", function () {
     expect(items[0].sell_in).toBe(9);
     expect(items[0].quality).toBe(19);
   });
+
+  it("does not decrease quality below 0", function () {
+    items.push(new Item("Normal Item", 10, 0));
+
+    update_quality();
+
+    expect(items[0].quality).toBe(0);
+  });
 });
